37d43e7332df2949f4622b214ba1388b4bd6568b
Roni <roni.c.castro@gmail.com>
2020-04-04 22:20:03 -0300

    Add get project by id

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ app.get('/projects', async (req, res) => {
   res.json({ projects })
 })
 
+app.get('/projects/:id', async (req, res) => {
+  const project = projects.find(project => project.id === req.params.id)
+  res.json(project)
+})
+
 module.exports = app
--- server.test.js
+++ server.test.js
@@ -3,9 +3,20 @@ const app = require('./server')
 
 const request = supertest(app)
 
-test('GET projects', async done => {
-  const response = await request.get('/projects')
-  expect(response.status).toBe(200)
-  expect(response.body.projects.length).toBe(0)
-  done()
+describe('GET /projects', () => {
+  test('returns with json containing projects object', async done => {
+    const response = await request.get('/projects')
+    expect(response.status).toBe(200)
+    expect(response.body.projects.length).toBe(0)
+    done()
+  })
+})
+
+describe('GET /projects/:id', () => {
+  it('respond with json containing a single project', async done => {
+    const response = await request.get('/projects/1')
+    expect(response.status).toBe(200)
+    expect(response.body.length).toBe(0)
+    done()
+  })
 })
