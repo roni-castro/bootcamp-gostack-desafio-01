da3b845590ce39dfe2df6e162b15f17c326e429a
Roni <roni.c.castro@gmail.com>
2020-04-04 23:14:28 -0300

    check for required fields

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let projects = []
 
 const mockDb = (projectsMock) => {
@@ -9,8 +11,15 @@ const mockDb = (projectsMock) => {
 
 const checkNotFoundId = (req, res, next) => {
   const project = projects.find(project => project.id === req.params.id)
-  if(!project) {
-    res.status(404).send({ error: 'Project not found'})
+  if (!project) {
+    return res.status(404).send({ error: 'Project not found' })
+  }
+  next()
+}
+
+const checkRequiredFields = (req, res, next) => {
+  if (!req.body.title || !req.body.id) {
+    return res.status(400).send({ error: 'title or id is missing' })
   }
   next()
 }
@@ -22,6 +31,12 @@ app.get('/projects', async (req, res) => {
 app.get('/projects/:id', checkNotFoundId, async (req, res) => {
   const project = projects.find(project => project.id === req.params.id)
   res.json(project)
+})
+
+app.post('/projects', checkRequiredFields, async (req, res) => {
+  const project = {tasks: [], ...req.body }
+  projects.push(project)
+  res.status(201).json(project)
 })
 
 app.mockDb = mockDb
--- server.test.js
+++ server.test.js
@@ -1,35 +1,75 @@
 const supertest = require('supertest')
 const app = require('./server')
+const uuid4 = require('uuid4')
 
-const projects = [{
+const projectsDefault = [{
   id: '1',
   title: 'Project 1',
   tasks: ['Nova tarefa']
 }]
-app.mockDb(projects)
 
-const request = supertest(app)
-
-describe('GET /projects', () => {
-  test('returns with json containing projects object', async done => {
-    const response = await request.get('/projects')
-    expect(response.status).toBe(200)
-    expect(response.body.projects.length).toBe(1)
-    done()
+describe('Projects', () => {
+  let projects
+  let request
+  beforeEach(() => {
+    projects = projectsDefault
+    app.mockDb(projects)
+    request = supertest(app)
   })
-})
 
-describe('GET /projects/:id', () => {
-  it('respond with json containing a single project', async done => {
-    const response = await request.get('/projects/1')
-    expect(response.status).toBe(200)
-    expect(response.body.id).toBe('1')
-    done()
+  describe('GET /projects', () => {
+    test('returns with json containing projects object', async done => {
+      const response = await request.get('/projects')
+      expect(response.status).toBe(200)
+      expect(response.body.projects.length).toBe(1)
+      done()
+    })
   })
-  it('respond with 404 error when id not found', async done => {
-    const response = await request.get('/projects/0')
-    expect(response.status).toBe(404)
-    expect(response.body.error).toBe('Project not found')
-    done()
+
+  describe('GET /projects/:id', () => {
+    it('respond with json containing a single project', async done => {
+      const response = await request.get('/projects/1')
+      expect(response.status).toBe(200)
+      expect(response.body.id).toBe('1')
+      done()
+    })
+    it('respond with 404 error when id not found', async done => {
+      const response = await request.get('/projects/0')
+      expect(response.status).toBe(404)
+      expect(response.body.error).toBe('Project not found')
+      done()
+    })
+  })
+
+  describe('POST /projects', () => {
+    it('respond with 201 created project', async done => {
+      const data = {
+        id: uuid4(),
+        title: 'Project created',
+        tasks: []
+      }
+      const response = await request.post('/projects').send(data)
+      expect(response.status).toBe(201)
+      expect(response.body).toMatchObject(data)
+      done()
+    })
+    describe('respond with Bad Request if not passed required fields', () => {
+      test.each([
+        [{ id: '11' }, 400],
+        [{ title: 'title'}, 400],
+        [{ title: 'title', tasks: []}, 400],
+      ])('post with body %o should return %i', async (data, expectedStatus) => {
+        const response = await request.post('/projects').send(data)
+        expect(response.status).toBe(expectedStatus)
+        expect(response.body.error).toBe('title or id is missing')
+      });
+    })
+    it('respond with Bad Request if not passed required id field', async done => {
+      const data = { title: 'title' }
+      const response = await request.post('/projects').send(data)
+      expect(response.status).toBe(400)
+      expect(response.body.error).toBe('title or id is missing')
+      done()
+    })
   })
 })
