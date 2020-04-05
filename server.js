1aa1699d55abd42b7e2477d1a045f8453f2f33ed
Roni <roni.c.castro@gmail.com>
2020-04-04 22:39:04 -0300

    Add not found middleware

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ const app = express()
 
 let projects = []
 
+const mockDb = (projectsMock) => {
+  projects = projectsMock
+}
+
+const checkNotFoundId = (req, res, next) => {
+  const project = projects.find(project => project.id === req.params.id)
+  if(!project) {
+    res.status(404).send({ error: 'Project not found'})
+  }
+  next()
+}
+
 app.get('/projects', async (req, res) => {
   res.json({ projects })
 })
 
-app.get('/projects/:id', async (req, res) => {
+app.get('/projects/:id', checkNotFoundId, async (req, res) => {
   const project = projects.find(project => project.id === req.params.id)
   res.json(project)
 })
+
+app.mockDb = mockDb
 
 module.exports = app
--- server.test.js
+++ server.test.js
@@ -1,13 +1,20 @@
 const supertest = require('supertest')
 const app = require('./server')
 
+const projects = [{
+  id: '1',
+  title: 'Project 1',
+  tasks: ['Nova tarefa']
+}]
+app.mockDb(projects)
+
 const request = supertest(app)
 
 describe('GET /projects', () => {
   test('returns with json containing projects object', async done => {
     const response = await request.get('/projects')
     expect(response.status).toBe(200)
-    expect(response.body.projects.length).toBe(0)
+    expect(response.body.projects.length).toBe(1)
     done()
   })
 })
@@ -16,7 +23,13 @@ describe('GET /projects/:id', () => {
   it('respond with json containing a single project', async done => {
     const response = await request.get('/projects/1')
     expect(response.status).toBe(200)
-    expect(response.body.length).toBe(0)
+    expect(response.body.id).toBe('1')
+    done()
+  })
+  it('respond with 404 error when id not found', async done => {
+    const response = await request.get('/projects/0')
+    expect(response.status).toBe(404)
+    expect(response.body.error).toBe('Project not found')
     done()
   })
 })
